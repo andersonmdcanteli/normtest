da90368499345c7ad9c3f2ae147bb5293029a5a9
Anderson Marcos Dias Canteli <andersonmdcanteli@gmail.com>
2023-10-04 20:14:44 -0300

    Update normtest.py

--- normtest/normtest.py
+++ normtest/normtest.py
@@ -4,10 +4,17 @@
 
 ## Functions WITH good TESTS ###
 - rj_critical_value(n, alpha=0.05)
+- rj_p_value(statistic, n)
+- ryan_joiner(x_data, alpha=0.05, method="blom", weighted=False)
 
 ## Functions WITH some TESTS ###
+- rj_correlation_plot(axes, x_data, method="blom", weighted=False)
+- rj_dist_plot(axes, x_data, method="blom", min=4, max=50, deleted=False, weighted=False)
+
+- make_heatmap(axes, df, n_samples, alpha_column_name=None, n_rep_name=None, tests_column_names=None, normal=True, safe=False)
+- normal_distribution_plot(axes, n_rep, seed=None, xinfo=[0.00001, 0.99999, 1000], loc=0.0, scale=1.0, safe=False)
 - ordered_statistics(n, method)
-- ryan_joiner(x_data, alpha=0.05, method="ryan-joiner")
+
 
 ## Functions WITHOUT tests ###
 
@@ -21,7 +28,7 @@ Author: Anderson Marcos Dias Canteli <andersonmdcanteli@gmail.com>
 
 Created: September 22, 2023.
 
-Last update: September 27, 2023
+Last update: October 04, 2023
 
 
 
@@ -50,15 +57,6 @@ from .utils import constants
 ##### CLASS #####
 
 ##### FUNCTIONS #####
-# rj_critical_value(n, alpha=0.05)
-# ryan_joiner(x_data, alpha=0.05, method="blom", weighted=False)
-# rj_correlation_plot(axes, x_data, method="blom", weighted=False)
-# rj_dist_plot(axes, x_data, method="blom", min=4, max=50, deleted=False, weighted=False)
-# rj_dist_plot(axes, x_data, method="blom", min=4, max=50, deleted=False, weighted=False)
-# rj_p_value(statistic, n)
-# ordered_statistics(n, method)
-# normal_distribution_plot(axes, n_rep, seed=None, xinfo=[0.00001, 0.99999, 1000], loc=0.0, scale=1.0, safe=False)
-
 
 
 
